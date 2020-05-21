2395cba5672ad6067a35e551d0faa1a2ccc85b73
avetyansos@yahoo.com <@vet!an1989ASD>
2020-05-21 13:50:39 +0400

    Initial Commit

---
+++ SmartClickAITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartClickAITest
+//
+//  Created by Sos Avetyan on 5/21/20.
+//  Copyright © 2020 Sos Avetyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
